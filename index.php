6fc5f1888ed06d7ac050df3b01985476992cced9
Louis-Philippe Cloutier <louisphilippe.cloutier@hooktstudios.com>
2013-11-19 11:59:19 -0500

    Ajout du html5shiv pour un support des navigateurs ne supportant pas les balises html5

--- index.php
+++ index.php
@@ -32,6 +32,10 @@ if(isset($_GET['lecture'])) {
     <meta charset="utf-8">
     <title>Classiques de la littérature québécoise</title>
     <link rel="stylesheet" href="css/styles.css">
+
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <![endif]-->
   </head>
   <body>
     <?php if($oeuvre): ?>
