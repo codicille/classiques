33de667c4772b0eb1483787bb384619cb2a427ce
Louis-Philippe Cloutier <louisphilippe.cloutier@hooktstudios.com>
2013-11-21 11:15:08 -0500

    Ajout d'une meta permettant la restriction du viewport au proportion du périphérique

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,6 @@
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0, user-scalable=no">
   <title>Classiques de la littérature québécoise</title>
   <link rel="stylesheet" href="/css/styles.css">
 
