98b9151dab6155c1fd0da108a8dfd75687b44431
Louis-Philippe Cloutier <louisphilippe.cloutier@hooktstudios.com>
2013-11-27 12:04:07 -0500

    Ajout d'un pied de page collant

--- fiche.php
+++ fiche.php
@@ -18,52 +18,53 @@ if(isset($_GET['oeuvre'])) {
 <html lang="fr">
   <?php require_once('includes/head.php') ?>
   <body>
-    <div class="header-outer">
-      <header>
-        <a href="/" class="top-banner">
-          <span class="back-wrap"><span class="back">Revenir à la liste des œuvres</span></span>
-          <span class="site-title">Classiques de la littérature québécoise</span>
-        </a>
-        <div class="intro">
-          <h1><?php echo $oeuvre->titre; ?></h1>
-          <h2><?php echo $oeuvre->auteur; ?></h2>
-        </div>
-      </header>
-    </div>
-
-    <div class="main-outer">
-      <div class="main">
-        <div class="book-wrap">
-          <div class="book-infos">
-            <a class="flat-btn-preview heading" href="/<?php echo $_GET['oeuvre'] ?>">Lire maintenant</a>
-            <h3>Description</h3>
-            <p><?php echo $oeuvre->description ?></p>
-
-            <?php if (is_array($oeuvre->etudes)): ?>
-              <h4>Choix d'études</h4>
-              <?php foreach($oeuvre->etudes as $etude): ?>
-                <?php
-                  # Mise en forme du nom de l'ouvrage
-                  if(isset($etude[1])) $etude[1] = "<em>$etude[1]</em>"
-                ?>
-                <p><?php echo join($etude, ', ') ?></p>
-              <?php endforeach ?>
-            <?php endif ?>
-
-            <?php if (is_object($oeuvre->notes_edition)): ?>
-              <h4>Note sur l’édition</h4>
-              <?php foreach($oeuvre->notes_edition as $nom => $note): ?>
-                <?php if (strpos($note, '/') === 0 || strpos($note, 'http') === 0): ?>
-                  <p><a target="_blank" href="<?php echo $note ?>"><?php echo $nom ?></a></p>
-                <?php else: ?>
-                  <p><?php echo $nom ?> : <?php echo $note ?></p>
-                <?php endif ?>
-              <?php endforeach ?>
-            <?php endif ?>
+    <div class="wrap">
+      <div class="header-outer">
+        <header>
+          <a href="/" class="top-banner">
+            <span class="back-wrap"><span class="back">Revenir à la liste des œuvres</span></span>
+            <span class="site-title">Classiques de la littérature québécoise</span>
+          </a>
+          <div class="intro">
+            <h1><?php echo $oeuvre->titre; ?></h1>
+            <h2><?php echo $oeuvre->auteur; ?></h2>
           </div>
-          <div class="book-preview">
-            <img class="book-thumb" src="/oeuvres/img/<?php echo $oeuvre_param ?>.jpg">
-            <a class="flat-btn-preview" href="/<?php echo $_GET['oeuvre'] ?>">Lire maintenant</a>
+        </header>
+      </div>
+
+      <div class="main-outer">
+        <div class="main">
+          <div class="book-wrap">
+            <div class="book-infos">
+              <a class="flat-btn-preview heading" href="/<?php echo $_GET['oeuvre'] ?>">Lire maintenant</a>
+              <h3>Description</h3>
+              <p><?php echo $oeuvre->description ?></p>
+
+              <?php if (is_array($oeuvre->etudes)): ?>
+                <h4>Choix d'études</h4>
+                <?php foreach($oeuvre->etudes as $etude): ?>
+                  <?php
+                    # Mise en forme du nom de l'ouvrage
+                    if(isset($etude[1])) $etude[1] = "<em>$etude[1]</em>"
+                  ?>
+                  <p><?php echo join($etude, ', ') ?></p>
+                <?php endforeach ?>
+              <?php endif ?>
+
+              <?php if (is_object($oeuvre->notes_edition)): ?>
+                <h4>Note sur l’édition</h4>
+                <?php foreach($oeuvre->notes_edition as $nom => $note): ?>
+                  <?php if (strpos($note, '/') === 0 || strpos($note, 'http') === 0): ?>
+                    <p><a target="_blank" href="<?php echo $note ?>"><?php echo $nom ?></a></p>
+                  <?php else: ?>
+                    <p><?php echo $nom ?> : <?php echo $note ?></p>
+                  <?php endif ?>
+                <?php endforeach ?>
+              <?php endif ?>
+            </div>
+            <div class="book-preview">
+              <img class="book-thumb" src="/oeuvres/img/<?php echo $oeuvre_param ?>.jpg">
+              <a class="flat-btn-preview" href="/<?php echo $_GET['oeuvre'] ?>">Lire maintenant</a>
           </div>
         </div>
       </div>
--- index.php
+++ index.php
@@ -6,34 +6,36 @@
   require_once('includes/head.php')
   ?>
   <body>
-    <div class="header-outer">
-      <header>
-        <div class="top-banner">
-          <h1>Classiques de la littérature québécoise</h1>
-        </div>
-        <div class="intro">
-          <p class="desc">
-            Les Classiques de la littérature québécoise visent à rendre disponibles au grand public
-            des versions numériques validées des œuvres libres de droit du corpus littéraire québécois. </p>
-          <p class="about-btn-wrap">
-            <a href="/a-propos" class="flat-btn">À propos</a>
-          </p>
-        </div>
-      </header>
-    </div>
+    <div class="wrap">
+      <div class="header-outer">
+        <header>
+          <div class="top-banner">
+            <h1>Classiques de la littérature québécoise</h1>
+          </div>
+          <div class="intro">
+            <p class="desc">
+              Les Classiques de la littérature québécoise visent à rendre disponibles au grand public
+              des versions numériques validées des œuvres libres de droit du corpus littéraire québécois. </p>
+            <p class="about-btn-wrap">
+              <a href="/a-propos" class="flat-btn">À propos</a>
+            </p>
+          </div>
+        </header>
+      </div>
 
-    <div class="main-outer">
-      <div class="main">
-        <ul class="classy-list">
-          <?php foreach($bibliotheque->oeuvres as $tag => $oeuvre): ?>
-          <li>
-            <a href="/fiche/<?php echo $tag ?>" class="classy-list-item">
-              <span class="classy-list-title"><?php echo $oeuvre->titre; ?></span>
-              <span class="classy-list-desc"><?php echo $oeuvre->auteur; ?></span>
-            </a>
-          </li>
-          <?php endforeach ?>
-        </ul>
+      <div class="main-outer">
+        <div class="main">
+          <ul class="classy-list">
+            <?php foreach($bibliotheque->oeuvres as $tag => $oeuvre): ?>
+            <li>
+              <a href="/fiche/<?php echo $tag ?>" class="classy-list-item">
+                <span class="classy-list-title"><?php echo $oeuvre->titre; ?></span>
+                <span class="classy-list-desc"><?php echo $oeuvre->auteur; ?></span>
+              </a>
+            </li>
+            <?php endforeach ?>
+          </ul>
+        </div>
       </div>
     </div>
 
